60484b4e1da0c3b0b64dc68303be75cfec3fd854
ivancostilla <ivan20punto88@gmail.com>
2020-12-03 17:31:43 -0300

    creando cards dinamicas con js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector(".navbar__hamburger");
 const navLinks = document.querySelector(".navbar");
-const imgPopup = document.querySelectorAll('.galeria__img');
-const imgCard = document.querySelectorAll('.card__img');
+const imgPopup = document.querySelectorAll(".galeria__img");
+
 /* menu responsive hamburguesa */
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
@@ -23,20 +23,19 @@ $(window).on('scroll', function() {
     };
 });
 
-/* galeria popup */
-imgPopup.forEach(popup => {
-    popup.addEventListener('click', () => {
-        popup.classList.toggle('popup');
-    });
-});
+/* funcion para asignar la clase popup
+ a distintas variables con distintos elementos html */
+const asignarPopup = (elem) => {
+        (elem).forEach(popup => {
+            popup.addEventListener('click', () => {
+                popup.classList.toggle('popup');
+            });
+        });
+    }
+    /* galeria popup */
+asignarPopup(imgPopup);
 
-/* imagenes en las cards */
-imgCard.forEach(popup => {
-    popup.addEventListener('click', () => {
-        popup.classList.toggle('popup');
-    });
-});
-
+/* img de portada dinamica, que cambia cada 4 segundos */
 function cambiarBg() {
     /* si solo pongo la url, no me toma la propiedad bg attchment en css, por eso coloco todo */
     const images = [` url(../img/hero1.webp) no-repeat 75% 75% / cover fixed, linear-gradient(120deg, hsla(337, 69%, 55%, .4) 0%, hsla(0, 88%, 47%, .4) 100%)`,
@@ -50,3 +49,76 @@ function cambiarBg() {
     head.style.background = bg;
 };
 setInterval(cambiarBg, 4000);
+
+/* funcion para agregar la info a las cards dinamicamente */
+function cardsInfo(img, nombre, descripcion) {
+    this.img = `img/${img}.webp`;
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    /* genero precios random: */
+    let precio = Math.round(Math.random() * 700 + 100);
+    this.precio = `$${precio}`;
+    /* asigno id random por ahora, para pruebas, esas id van a servir para agrgar el producto al carrito */
+    let id = Math.round(Math.random() * 1000);
+    this.id = `${id}`;
+};
+/* creamos el contenido de las cards: */
+const papasFritas = new cardsInfo("papas", "Papas Fritas", "Papas fritas con salsa especial de la casa.");
+const nachos = new cardsInfo("nachos", "Nachos", "Nachos con salsa acida, barbacoa, cheddar y verdeo.");
+const chocloFrito = new cardsInfo("choclo", "Choclo Frito", "Choclo frito con salsa especial de la casa.");
+const burga1 = new cardsInfo("burga", "Burga 1", "burga con salsa especial de la casa.");
+const burga2 = new cardsInfo("burga", "Burga 2", "burga con salsa especial de la casa.");
+const burga3 = new cardsInfo("burga", "Burga 3", "burga con salsa especial de la casa.");
+const burga4 = new cardsInfo("burga", "Burga 3", "burga con salsa especial de la casa.");
+const burga5 = new cardsInfo("burga", "Burga 3", "burga con salsa especial de la casa.");
+const burga6 = new cardsInfo("burga", "Burga 3", "burga con salsa especial de la casa.");
+
+
+/* guardamos el contenido de cada card en un array */
+let cardsBurgasArray = [burga1, burga2, burga3, burga4, burga5, burga6];
+let cardsExtrasArray = [papasFritas, nachos, chocloFrito];
+
+/* guardamos en otro array los array anteriores por si tenemos mas secciones html
+que contengan cards: */
+let cardsContainer = [cardsBurgasArray, cardsExtrasArray];
+
+/* guardamos en variables los contenedores padre de las cards: */
+const cardBurgas = document.querySelector(".card__container-burgas");
+const cardExtras = document.querySelector(".card__container-extras");
+
+// Crear un arreglo de contenedores para asociar con las cards
+const containers = [cardBurgas, cardExtras];
+
+/* hasta acá funciona todo bien, el problema es cuando entro al for, se me agregan las todas las cards a las 2 secciones, lo que yo quiero es que las cards guardadas en cardsBurgasArray se guarden en la seccion cardBurgas y las ars guaradas rn arsxtrasArray se guarden en la seccion cardExtras
+puedo hacerlo por separado pero seria repetir codigo.. aiuda! */
+/* creamos dinamicamente las cards: */
+for (i = 0; i < cardsContainer.length; i++) {
+    // Inicializar código dentro del ciclo
+    let htmlCode = "";
+    for (cards in cardsContainer[i]) {
+        let card = cardsContainer[i][cards];
+        let img = card["img"];
+        let nombre = card["nombre"];
+        let descripcion = card["descripcion"];
+        let precio = card["precio"];
+        let id = card["id"];
+        htmlCode += `
+<div class="card">
+<img class="card__img" src="${img}" alt="">
+<h3 class="card__h3">${nombre}</h3>
+<p class="card__content">${descripcion}<span class="card__precio">${precio}</span></p>
+<button class="card__button" id="${id}">Agregar al carrito</button>
+</div>
+`;
+    };
+    // Asignar HTML a contenedor
+    containers[i].innerHTML = htmlCode;
+}
+
+
+/* agregando la clase popup a las img de las cards, 
+ la coloco aqui xq tengo que invocar la funcion despues de haber
+ creado las cards dinamicamente con js */
+const imgCard = document.querySelectorAll(".card__img");
+/* si pongo la variable arriba de todo, no me toma el evento */
+asignarPopup(imgCard);
